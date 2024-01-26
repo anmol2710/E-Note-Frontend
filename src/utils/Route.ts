74c9f19f4c511323190cc4506fc1a9020f5cd4ca
Anmol Garg <anmolgarg271005@gmail.com>
2024-01-26 19:52:22 +0530

    "Initial commit"

--- src/utils/Route.ts
+++ src/utils/Route.ts
@@ -1,3 +1,3 @@
-const host = "http://localhost:4000"
+const host = "https://e-note-backend-1jq9.onrender.com/"
 export const signUpRoute = `${host}/user/signup`
 export const loginRoute = `${host}/user/login`
