7c9919d5049ffbec9cd31535c34b2af1605e8113
anmol2710 <anmolgarg271005@gmail.com>
2024-03-05 12:44:50 +0530

    "Some color changes"

--- src/components/Notes/PEE.tsx
+++ src/components/Notes/PEE.tsx
@@ -9,7 +9,7 @@ const PEE = () => {
                     <div className="card-body">
                         <h3 className="card-title text-2xl">Principles of Electrical Engineering (PEE)</h3>
                         <div className="container div">
-                            <Link to="/notes/PEE" className="btn btn-primary bg-indigo-600">Notes</Link>
+                            <Link to="/notes/PEE" className="btn btn-primary">Notes</Link>
                             <Link to="/Lab-notes/PEE" className="btn btn-primary">Lab Work</Link>
                         </div>
                     </div>
